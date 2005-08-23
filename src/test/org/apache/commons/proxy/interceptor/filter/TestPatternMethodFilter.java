4abf39be97df4ea4756a68704cbedd942f255082
James W. Carman <jcarman@apache.org>
2005-08-23 03:10:30 +0000

    Adding PatternMethodFilter
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/proxy/trunk@239308 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/proxy/interceptor/filter/TestPatternMethodFilter.java
@@ -0,0 +1,35 @@
+/*
+ *  Copyright 2005 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.commons.proxy.interceptor.filter;
+
+import junit.framework.TestCase;
+
+import java.util.Date;
+
+/**
+ * @author James Carman
+ * @version 1.0
+ */
+public class TestPatternMethodFilter extends TestCase
+{
+    public void testAccepts() throws Exception
+    {
+        final PatternMethodFilter filter = new PatternMethodFilter( "set\\w+|get\\w+" );
+        assertTrue( filter.accepts( Date.class.getMethod( "getSeconds" ) ) );
+        assertTrue( filter.accepts( Date.class.getMethod( "setSeconds", Integer.TYPE ) ) );
+        assertFalse( filter.accepts( Date.class.getMethod( "toString" ) ) );
+    }
+}
