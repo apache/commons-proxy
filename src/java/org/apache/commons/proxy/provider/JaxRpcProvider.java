11a75a2b1ae254a2218161ebcecbfe6e70d4f052
James W. Carman <jcarman@apache.org>
2005-08-25 05:26:30 +0000

    Adding JaxRpcProvider.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/proxy/trunk@239995 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/proxy/provider/JaxRpcProvider.java
@@ -0,0 +1,129 @@
+/*
+ *  Copyright 2005 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.commons.proxy.provider;
+
+import org.apache.commons.proxy.ObjectProvider;
+import org.apache.commons.proxy.exception.ObjectProviderException;
+
+import javax.xml.namespace.QName;
+import javax.xml.rpc.Service;
+import javax.xml.rpc.ServiceException;
+import javax.xml.rpc.ServiceFactory;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Returns a proxy for a JAX-RPC-based service.
+ *
+ * @author James Carman
+ * @version 1.0
+ */
+public class JaxRpcProvider<T> implements ObjectProvider<T>
+{
+    private final Class<? extends T> serviceInterface;
+    private String url;
+    private String serviceNamespaceUri;
+    private String serviceLocalPart;
+    private String servicePrefix;
+    private String portNamespaceUri;
+    private String portLocalPart;
+    private String portPrefix;
+
+    public JaxRpcProvider( Class<? extends T> serviceInterface )
+    {
+        this.serviceInterface = serviceInterface;
+    }
+
+    public T getObject()
+    {
+        try
+        {
+            final Service service = ( url == null ? ServiceFactory.newInstance().createService( getServiceQName() ) : ServiceFactory.newInstance().createService( new URL( url ), getServiceQName() ) );
+            final QName portQName = getPortQName();
+            return serviceInterface.cast( portQName == null ? service.getPort( serviceInterface ) : service.getPort( portQName, serviceInterface ) );
+        }
+        catch( ServiceException e )
+        {
+            throw new ObjectProviderException( "Unable to create JAX-RPC service proxy.", e );
+        }
+        catch( MalformedURLException e )
+        {
+            throw new ObjectProviderException( "Invalid URL given.", e );
+        }
+    }
+
+    private QName getQName( String namespaceUri, String localPart, String prefix )
+    {
+        if( namespaceUri != null && localPart != null && prefix != null )
+        {
+            return new QName( namespaceUri, localPart, prefix );
+        }
+        else if( namespaceUri != null && localPart != null )
+        {
+            return new QName( namespaceUri, localPart );
+        }
+        else if( localPart != null )
+        {
+            return new QName( localPart );
+        }
+        return null;
+    }
+
+    private QName getServiceQName()
+    {
+        return getQName( serviceNamespaceUri, serviceLocalPart, servicePrefix );
+    }
+
+    private QName getPortQName()
+    {
+        return getQName( portNamespaceUri, portLocalPart, portPrefix );
+    }
+
+    public void setPortNamespaceUri( String portNamespaceUri )
+    {
+        this.portNamespaceUri = portNamespaceUri;
+    }
+
+    public void setPortLocalPart( String portLocalPart )
+    {
+        this.portLocalPart = portLocalPart;
+    }
+
+    public void setPortPrefix( String portPrefix )
+    {
+        this.portPrefix = portPrefix;
+    }
+
+    public void setServiceNamespaceUri( String serviceNamespaceUri )
+    {
+        this.serviceNamespaceUri = serviceNamespaceUri;
+    }
+
+    public void setServiceLocalPart( String serviceLocalPart )
+    {
+        this.serviceLocalPart = serviceLocalPart;
+    }
+
+    public void setServicePrefix( String servicePrefix )
+    {
+        this.servicePrefix = servicePrefix;
+    }
+
+    public void setUrl( String url )
+    {
+        this.url = url;
+    }
+}
---
+++ src/test/org/apache/commons/proxy/provider/TestJaxRpcProvider.java
@@ -0,0 +1,32 @@
+/*
+ *  Copyright 2005 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.commons.proxy.provider;
+import junit.framework.TestCase;
+import org.apache.commons.proxy.util.QuoteService;
+
+public class TestJaxRpcProvider extends TestCase
+{
+    public void testGetObject() throws Exception
+    {
+        final JaxRpcProvider provider = new JaxRpcProvider<QuoteService>( QuoteService.class );
+        provider.setUrl( "http://services.xmethods.net/soap/urn:xmethods-delayed-quotes.wsdl" );
+        provider.setServiceNamespaceUri( "http://www.themindelectric.com/wsdl/net.xmethods.services.stockquote.StockQuote/" );
+        provider.setServiceLocalPart( "net.xmethods.services.stockquote.StockQuoteService" );
+        provider.setPortLocalPart( "net.xmethods.services.stockquote.StockQuotePort" );
+        final QuoteService quote = ( QuoteService )provider.getObject();
+        assertNotNull( quote );
+    }
+}
