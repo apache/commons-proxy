fd3ef03210f1fe378938cf624ea6b811aaa9cea8
James W. Carman <jcarman@apache.org>
2005-08-30 21:30:53 +0000

    Improving test coverage.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/proxy/trunk@264871 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/proxy/exception/AbstractExceptionClassTestCase.java
@@ -0,0 +1,65 @@
+/* $Id$
+ *
+ * Copyright 2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.proxy.exception;
+
+import junit.framework.TestCase;
+
+/**
+ * @author James Carman
+ * @version 1.0
+ */
+public abstract class AbstractExceptionClassTestCase extends TestCase
+{
+    private final Class<? extends Exception> exceptionClass;
+
+    public AbstractExceptionClassTestCase( Class<? extends Exception> exceptionClass )
+    {
+        this.exceptionClass = exceptionClass;
+    }
+
+    public void testNoArgConstructor() throws Exception
+    {
+        Exception e = exceptionClass.getConstructor().newInstance();
+        assertNull( e.getMessage() );
+        assertNull( e.getCause() );
+    }
+
+    public void testMessageOnlyConstructor() throws Exception
+    {
+        final String message = "message";
+        Exception e = exceptionClass.getConstructor( String.class ).newInstance( message );
+        assertEquals( message, e.getMessage() );
+        assertNull( e.getCause() );
+    }
+
+    public void testCauseOnlyConstructor() throws Exception
+    {
+        final Exception cause = new Exception();
+        Exception e = exceptionClass.getConstructor( Throwable.class ).newInstance( cause );
+        assertEquals( cause.toString(), e.getMessage() );
+        assertEquals( cause, e.getCause() );
+    }
+
+    public void testMessageAndCauseConstructor() throws Exception
+    {
+        final Exception cause = new Exception();
+        final String message = "message";
+        Exception e = exceptionClass.getConstructor( String.class, Throwable.class ).newInstance( message, cause );
+        assertEquals( message, e.getMessage() );
+        assertEquals( cause, e.getCause() );
+    }
+}
