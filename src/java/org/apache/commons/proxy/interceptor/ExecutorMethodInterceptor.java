15ad1a748f42d04b36bb3e10f34509d0aa469850
James W. Carman <jcarman@apache.org>
2005-09-05 14:51:08 +0000

    Implementing ExecutorMethodInterceptor
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/proxy/trunk@278760 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/proxy/interceptor/ExecutorMethodInterceptor.java
@@ -0,0 +1,66 @@
+/* $Id$
+ *
+ * Copyright 2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.proxy.interceptor;
+
+import org.aopalliance.intercept.MethodInvocation;
+
+import java.util.concurrent.Executor;
+
+/**
+ * A method interceptor that uses an {@link Executor} to execute the method invocation.
+ * <p />
+ * <b>Note</b>: Only <em>void</em> methods can be intercepted using this class!  Any attempts to intercept
+ * non-void methods will result in an {@link IllegalArgumentException}.  If the proxy interfaces include non-void
+ * methods, try using a {@link FilteredMethodInterceptor} to wrap an instance of this class.
+ * @author James Carman
+ * @version 1.0
+ */
+public class ExecutorMethodInterceptor extends AbstractMethodInterceptor
+{
+    private final Executor executor;
+
+    public ExecutorMethodInterceptor( Executor executor )
+    {
+        this.executor = executor;
+    }
+
+    public Object invoke( final MethodInvocation methodInvocation ) throws Throwable
+    {
+        if( Void.TYPE.equals( methodInvocation.getMethod().getReturnType() ) )
+        {
+            executor.execute( new Runnable()
+            {
+                public void run()
+                {
+                    try
+                    {
+                        methodInvocation.proceed();
+                    }
+                    catch( Throwable t )
+                    {
+                        getLog().error( "Method invocation threw an exception.", t );
+                    }
+                }
+            } );
+            return null;
+        }
+        else
+        {
+            throw new IllegalArgumentException( "Only void methods can be executed in a different thread." );
+        }
+    }
+}
---
+++ src/test/org/apache/commons/proxy/interceptor/TestExecutorMethodInterceptor.java
@@ -0,0 +1,105 @@
+/* $Id$
+ *
+ * Copyright 2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.proxy.interceptor;
+
+import junit.framework.TestCase;
+import org.apache.commons.proxy.factory.cglib.CglibProxyFactory;
+import org.apache.commons.proxy.util.Echo;
+import org.apache.commons.proxy.util.EchoImpl;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+
+public class TestExecutorMethodInterceptor extends TestCase
+{
+    public void testVoidMethod() throws Exception
+    {
+        final ExecutedEcho impl = new ExecutedEcho();
+        final OneShotExecutor executor = new OneShotExecutor();
+        final Echo proxy = ( Echo ) new CglibProxyFactory()
+                .createInterceptorProxy( impl, new ExecutorMethodInterceptor( executor ), Echo.class );
+        proxy.echo();
+        executor.getLatch().await();
+        assertEquals( executor.getThread(), impl.getExecutionThread() );
+    }
+
+    public void testNonVoidMethod() throws Exception
+    {
+        final ExecutedEcho impl = new ExecutedEcho();
+        final OneShotExecutor executor = new OneShotExecutor();
+        final Echo proxy = ( Echo ) new CglibProxyFactory()
+                .createInterceptorProxy( impl, new ExecutorMethodInterceptor( executor ), Echo.class );
+        try
+        {
+            proxy.echoBack( "hello" );
+            fail();
+        }
+        catch( IllegalArgumentException e )
+        {
+        }
+    }
+
+    public static class ExecutedEcho extends EchoImpl
+    {
+        private Thread executionThread;
+
+        public void echo()
+        {
+            executionThread = Thread.currentThread();
+        }
+
+        public Thread getExecutionThread()
+        {
+            return executionThread;
+        }
+    }
+
+    private static class OneShotExecutor implements Executor
+    {
+        private Thread thread;
+        private CountDownLatch latch = new CountDownLatch( 1 );
+
+        public void execute( final Runnable command )
+        {
+            thread = new Thread( new Runnable()
+            {
+                public void run()
+                {
+                    try
+                    {
+                        command.run();
+                    }
+                    finally
+                    {
+                        latch.countDown();
+                    }
+                }
+            } );
+            thread.start();
+        }
+
+        public Thread getThread()
+        {
+            return thread;
+        }
+
+        public CountDownLatch getLatch()
+        {
+            return latch;
+        }
+    }
+}
