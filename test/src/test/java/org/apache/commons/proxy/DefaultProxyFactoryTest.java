35d615228e1f151b716045fb6be29091f049dbea
Matthew Jason Benson <mbenson@apache.org>
2010-07-22 20:15:25 +0000

    add new test module to exercise the defaultProxyFactory
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/proxy/branches/version-2.0-work@966839 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/src/test/java/org/apache/commons/proxy/DefaultProxyFactoryTest.java
@@ -0,0 +1,52 @@
+package org.apache.commons.proxy;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Proxy;
+
+import org.apache.commons.proxy2.ProxyFactory;
+import org.apache.commons.proxy2.ProxyUtils;
+import org.apache.commons.proxy2.invoker.NullInvoker;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test the default ProxyFactory provided by {@link ProxyUtils}.
+ */
+public class DefaultProxyFactoryTest {
+    private ProxyFactory proxyFactory;
+
+    @Before
+    public void setUp() {
+        proxyFactory = ProxyUtils.proxyFactory();
+    }
+
+    @Test
+    public void testBasic() {
+        Foo foo = proxyFactory.createInvokerProxy(NullInvoker.INSTANCE,
+                Foo.class);
+        assertNotNull(foo);
+        assertTrue(foo instanceof Proxy);
+    }
+
+    @Test
+    public void testSubclassing() {
+        Bar bar = proxyFactory.createInvokerProxy(NullInvoker.INSTANCE,
+                Bar.class);
+        assertNotNull(bar);
+    }
+
+    @Test
+    public void testCombined() {
+        Bar bar = proxyFactory.createInvokerProxy(NullInvoker.INSTANCE,
+                Bar.class, Foo.class);
+        assertNotNull(bar);
+        assertTrue(bar instanceof Foo);
+    }
+
+    public interface Foo {
+    }
+
+    public static class Bar {
+    }
+}
