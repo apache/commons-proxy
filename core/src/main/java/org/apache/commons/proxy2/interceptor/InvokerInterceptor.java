7599b6807957ca4bb3df3ee5e929c0f7df753702
Matthew Jason Benson <mbenson@apache.org>
2013-09-21 17:23:24 +0000

    add invoker interceptor
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/proxy/branches/version-2.0-work@1525265 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/apache/commons/proxy2/interceptor/InvokerInterceptor.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.proxy2.interceptor;
+
+import org.apache.commons.lang3.Validate;
+import org.apache.commons.proxy2.Interceptor;
+import org.apache.commons.proxy2.Invocation;
+import org.apache.commons.proxy2.Invoker;
+
+/**
+ * Adapts an {@link Invoker} to the {@link Interceptor} interface.
+ */
+public class InvokerInterceptor implements Interceptor
+{
+    private static final long serialVersionUID = 1L;
+
+    private final Invoker invoker;
+
+    public InvokerInterceptor(Invoker invoker)
+    {
+        super();
+        this.invoker = Validate.notNull(invoker);
+    }
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable
+    {
+        return invoker.invoke(invocation.getProxy(), invocation.getMethod(), invocation.getArguments());
+    }
+
+}
